96ffffe16cfa49bad6b2fa54fbb881b5610c06aa
haenghaeng <haeng@pusan.ac.kr>
2025-09-30 16:44:40 +0900

    Add items
    애니메이션과 일부 연동된 기능을 제외하고 코인, 자석, 무적 아이템을 추가하였습니다.

---
+++ Assets/Scripts/Item/Invincible.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Invincible : MonoBehaviour, IItem
+{
+    private WaitForSeconds invincibleTime = new (5); // 5초 동안 무적
+
+    /// <summary>
+    /// 아이템 획득 시, 일정 시간동안 플레이어는 장애물로부터 피해를 받지 않습니다.
+    /// 무적 상태인 도중, 무적 아이템을 또 획득하였을 경우, 무적시간이 처음 부터 다시 시작됩니다
+    /// </summary>
+    public void UseItem(GameObject player)
+    {
+        // 플레이어가 무적상태인지 확인. 플레이어의 현재 상태를 저장한 곳에서 가져다 쓰는 게 좋을 듯
+        bool isInvincible = false;
+        // isInvincible = ???
+
+        // 무적상태라면 코루틴을 종료 후 재실행
+        if (isInvincible)
+        {
+            StopAllCoroutines();
+        }
+        StartCoroutine(TimerCoroutine());
+    }
+
+    private IEnumerator TimerCoroutine()
+    {
+        // 플레이어를 무적으로 변경
+        // Do something
+
+        // 논리값도 같이 변경
+        // Do something
+
+        // 5초 대기
+        yield return invincibleTime;
+
+        // 플레이어를 다시 원래대로 되돌림
+        // Do something
+
+        // 논리값도 원래대로 되돌림
+        // Do something
+    }
+}
---
+++ Assets/Scripts/Item/Magnet/Magnet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour, IItem
+{
+    [SerializeField] private GameObject CoinCheckerPrefab; // 플레이어에게 일시적으로 추가해 줄 코인 체커 프리팹
+
+    private WaitForSeconds magnetTime = new (5); // 5초 동안 당김
+
+    /// <summary>
+    /// 플레이어 주변의 코인을 플레이어 쪽으로 끌어들입니다.
+    /// </summary>
+    public void UseItem(GameObject player)
+    {
+        // 플레이어가 자석상태인지 확인. 플레이어의 현재 상태를 저장한 곳에서 가져다 쓰는 게 좋을 듯
+        bool isMagnet = false;
+
+        // 이미 자석 상태이면 처음부터 타이머를 다시 시작
+        if (isMagnet)
+        {
+            StopAllCoroutines();            
+            Destroy(player.transform.Find("CoinChecker")); // 플레이어 가진 코인 체커를 제거
+        }
+        StartCoroutine(TimerCoroutine(player));
+    }
+
+    private IEnumerator TimerCoroutine(GameObject player)
+    {
+        // 플레이어에게 코인 체커 추가
+        Instantiate(CoinCheckerPrefab, player.transform);
+
+        // 논리값도 같이 변경
+        // Do something
+
+        // 5초 대기
+        yield return magnetTime;
+
+        // 플레이어가 가진 코인 체커를 제거
+        Destroy(player.transform.Find("CoinChecker"));
+
+        // 논리값도 원래대로 되돌림
+        // Do something
+    }
+}
