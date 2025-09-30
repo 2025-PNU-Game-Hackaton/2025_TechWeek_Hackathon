bfc82eeb1d0984b636f7bc660268cd7d84922cb2
haenghaeng <haeng@pusan.ac.kr>
2025-09-30 18:53:24 +0900

    Fix conflict

--- Assets/Scripts/ItemEater.cs
+++ Assets/Scripts/ItemEater.cs
@@ -5,7 +5,7 @@ public class ItemEater : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         IItem item;
-        if (other.TryGetComponent<IItem>(out item))
+        if (other.TryGetComponent(out item))
         {
             item.UseItem(gameObject);
         }
